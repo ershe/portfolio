15d59952675b642e1b3887704ee2b388a4341bd1
kyartscouncil <97762840+kyartscouncil@users.noreply.github.com>
2022-10-16 14:00:44 -0400

    Proper BUILD accomplished

---
+++ src/views/ViewGallery.vue
@@ -0,0 +1,7 @@
+<template>
+	<div><h1>Gallery</h1></div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/ViewHome.vue
@@ -0,0 +1,12 @@
+<template>
+	<section class="container text-white">
+		<h2>Home Start</h2>
+		<ViewGallery></ViewGallery>
+	</section>
+</template>
+
+<script setup>
+	import ViewGallery from './ViewGallery.vue';
+</script>
+
+<style lang="scss" scoped></style>
